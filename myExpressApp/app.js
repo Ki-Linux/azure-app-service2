1857475f3113b57fb2111389b9b3e9ec29dc0f61
seima <linuxseima@gmail.com>
2021-11-30 19:16:15 +0900

    install bcrypt

--- myExpressApp/app.js
+++ myExpressApp/app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const mysql = require('mysql');
+const bcrypt = require('bcrypt');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
